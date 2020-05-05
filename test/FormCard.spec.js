628727ef3bbc1f24dcf5246085513e2a57abd3b4
Ryujiro Sakai <sakateu@gmail.com>
2020-05-05 17:00:24 +0900

    fix test

--- test/FormCard.spec.js
+++ test/FormCard.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import FormCard from '@/components/FormCard.vue'
 
-describe('Logo', () => {
+describe('FormCard', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(FormCard)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
